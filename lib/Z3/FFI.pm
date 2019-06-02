a44f2d70220a17a1bb856a13859fe8ecae68097a
Ryan Voots <simcop2387@simcop2387.info>
2019-06-02 07:12:13 -0400

    Loads the compiled/installed version of z3 from Alien::Z3

--- lib/Z3/FFI.pm
+++ lib/Z3/FFI.pm
@@ -1,3 +1,19 @@
 package Z3::FFI;
 
+use Moo;
+use warnings;
+
+use Data::Dumper;
+use FFI::Platypus;
+use FFI::CheckLib qw//;
+use File::ShareDir qw/dist_dir/;
+use Path::Tiny;
+
+my $search_path = path(dist_dir('Alien-Z3'))->child('dynamic');
+my $ffi_lib = FFI::CheckLib::find_lib_or_die(lib => 'z3', libpath => $search_path);
+my $ffi = FFI::Platypus->new();
+$ffi->lib($ffi_lib);
+
+
+
 1;
